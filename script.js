a061341c102efb594890620cd89db0fdf63bf91a
ScR420 <gamecheckboss@hotmail.com>
2025-08-24 20:19:55 +0200

    next3

--- script.js
+++ script.js
@@ -744,7 +744,6 @@ class ELOSimulator {
 document.addEventListener('DOMContentLoaded', () => {
     new ELOSimulator();
 });
-
 // Add some additional CSS for tournament results
 const additionalStyles = `
     .group-results {
@@ -911,3 +910,4 @@ const additionalStyles = `
 const styleSheet = document.createElement('style');
 styleSheet.textContent = additionalStyles;
 document.head.appendChild(styleSheet);
+
